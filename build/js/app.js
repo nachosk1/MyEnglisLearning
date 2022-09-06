d0ff5b8970651972baae7f907d6e2f050e7f4343
nachosk1 <pedroale328@gmail.com>
2022-09-06 15:26:16 -0300

    login

---
+++ build/js/app.js
@@ -0,0 +1,2 @@
+document.querySelectorAll(".input-login").forEach(e=>{e.addEventListener("blur",e=>{""!=e.target.value?e.target.nextElementSibling.classList.add("filled"):e.target.nextElementSibling.classList.remove("filled")})});
+//# sourceMappingURL=app.js.map
